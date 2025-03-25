50df0e6524827ac2b35138ce10c77129bacb06da
Rudrakumar Patel <rudrapatel2992003@gmail.com>
2025-03-25 09:05:56 +0530

    Add Swagger

--- routes/allListingRoute.js
+++ routes/allListingRoute.js
@@ -4,7 +4,6 @@ import verifyToken from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-@swagger
 router.get("/allHolidayPackages",allHolidayPackages);
 router.get("/searchHolidayPackages",searchHolidayPackages);
 
